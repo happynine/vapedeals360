7d9f2a88e731c63360e0c846ae1173e5ffbc925e
happynine <491374665@qq.com>
2026-07-06 21:19:39 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ const nextConfig: NextConfig = {
     root: path.resolve(__dirname),
   },
   allowedDevOrigins: ['*.dev.coze.site'],
+  output: 'standalone',
   experimental: {
     serverActions: {
       bodySizeLimit: '50mb',
